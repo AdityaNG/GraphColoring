b885c483036980ae64d9f5dada6ea0559c1db1b3
AdityaNG <adityang5@gmail.com>
2022-03-22 21:13:12 +0530

    Basic pthreads implementation

--- p2i.py
+++ p2i.py
@@ -13,10 +13,74 @@ def dec_to_base(num,base):  #Maximum base - 36
     base_num = base_num[::-1]  #To reverse the string
     return base_num
 
+class Mapper:
+    def __init__(self, v) -> None:
+        self.v = v
+        self.l = 0
+        for i in range(1, self.v+1):
+            self.l += i**self.v
+    
+    def __len__(self):
+        # 1**v + 2**v + ... + v**v
+        return self.l
 
-v = 3 # Number of vertices 
-for c in range(1, v+1):
-    print('colors : ', c)
-    for i in range(c**v):
-        print(' ', 'index : ', i, '\t->', [c,i] ,'->\t', dec_to_base(i, c).zfill(v))
+    def __getitem__(self, key):
+        if key > self.l:
+            raise IndexError
         
+        key+=1
+        t = 1
+        c = 1
+        while key>t:
+            key -= t
+            c+=1
+            t = c**self.v
+        
+        key -= 1
+
+        return key, c, dec_to_base(key, c).zfill(self.v)
+
+    def __setitem__(self, key, value):
+        print("set")
+
+
+def get_length(v):
+    l = 0
+    for i in range(1, v+1):
+        l += i**v
+    return l
+
+def get_item(v, key):
+    l = get_length(v)
+    if key > l:
+        raise IndexError
+        
+    key+=1
+    t = 1
+    c = 1
+    while key>t:
+        key -= t
+        c+=1
+        t = c**v
+        
+    key -= 1
+
+    return key, c, dec_to_base(key, c).zfill(v)
+
+v = 3
+
+m = Mapper(2)
+l = get_length(v)
+for i in range(l):
+    j, c, col = get_item(v, i)
+    print('index : ', i, '\t->', [c,j] ,'->\t', col)
+
+exit()
+
+v = 3
+
+m = Mapper(2)
+print(len(m))
+for i in range(len(m)):
+    j, c, col = m[i]
+    print('index : ', i, '\t->', [c,j] ,'->\t', col)
--- src/BruteForcePThread/main.cpp
+++ src/BruteForcePThread/main.cpp
@@ -1,137 +1,183 @@
+/* 
+ * Thread Pool
+ */
+
+#include <stdio.h>
 #include <iostream>
-#include <algorithm>
-#include <cstring>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
 #include <vector>
 #include <set>
-#include <limits>
+#include <map>
+#include <queue>
+#include <deque>
 
-using namespace std;
-const int V = 4;   //vertex no.
+#include <utility>
 
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <cstdlib>
+#include <signal.h>
 
-const int colors[V] = { 0,1,2, 4 };
-int counter = 0;
-bool problem = false;
+#include <math.h>
+#include <cassert>
 
-/* 
+#include <bits/stdc++.h>
 
-	 Example graph
+#define MAXTHREADS 4
+#define THREAD_TERMINATE_SIG -10
+#define LOG_DATA true
 
-		 2      7---------8
-		/ \      \        |
-       /   \      \       |
-      /     \      \      |
-     1------3-------5-----9
-     \     /       /      |
-      \   /       /       |
-       \ /       /        |
-        4-------6---------10
+std::vector<std::vector <int>> graph;
 
-*/
+// List of thread IDs 
+std::vector<pthread_t> THREADS(MAXTHREADS); // Actual Thread IDs
+// TODO : Look into replacing this with malloc
+std::vector < std::pair<int,int> > t_thread_numbers(MAXTHREADS); // Virtual Thread IDs
 
-int rate_list[V]; //{ 0,0,0,0,0,0,0,0,0,0};
+std::string decToBase(int num, int base) {
+  if(num==0)
+    return "0";
 
-int min_colors = std::numeric_limits<int>::max();
+  std::string base_num = "";
+  while (num>0) {
+    int dig = int(num%base);
+    if(dig<10) {
+      base_num += std::to_string(dig);
+    } else {
+      base_num += std::string(1, char('A'+dig-10));  // Using uppercase letters
+    }
+    num = num/base;
+  }
+  std::reverse(base_num.begin(), base_num.end());// To reverse the string
+  return base_num;
+}
 
-struct Graph_t {
-	int verteV_rates[V];
-	bool adj[V][V];
-	int colors[V];
-	bool colored[V];
-};
+std::string padZeros(std::string s, int n) {
+  while (s.length() < n) {
+    s = "0" + s;
+  }
+  return s;
+}
 
-bool isSafeToColor(vector < vector < int >> & graph, vector < int > color) {
+int getLength(int v) {
+  int l = 0;
+  for (int i=1; i<=v; i++) {
+    l += pow(i, v);
+  }
+  return l;
+}
+
+std::pair<int,int> getItem(int v, int key) {
+  int l = getLength(v);
+  assert(key <= l);
+
+  key+=1;
+  int t = 1;
+  int c = 1;
+  while (key>t) {
+    key-=t;
+    c+=1;
+    t = pow(c, v);
+  }
+        
+  key -= 1;
+
+  return std::make_pair(key, c);
+}
+
+bool isSafeToColor(std::vector<std::vector<int>> graph, std::vector <int> color) {
+  int V = color.size();
   for (int i = 0; i < V; i++)
     for (int j = i + 1; j < V; j++)
       if (graph[i][j] == 1 && color[j] == color[i])
         return false;
   return true;
 }
- 
-void printColorArray(vector < int > color) {
-  cout << ("Solution colors are: ") << endl;
 
-  set<int> s;
-  //min_colors
+/*
+ * Counter to decide which thread to 
+ * allocate the incoming request to 
+ */
+int thread_count = 0;
 
-  for (int i = 0; i < color.size(); i++) {
-    cout << (color[i]);
-	s.insert(color[i]);
-  }
-  
-  if (s.size() < min_colors) {
-	min_colors = s.size();
-  }
+/*
+ * DataArg handler for thread_number
+ */
+void * handleDataArg(void * args) {
 
-  cout << endl;
-}
-bool graphColoring(vector < vector < int >> & graph, int m, int i, vector < int > color) {
-	//cout << m << '\t' << i << endl;
-  if (i == V) {
-    if (isSafeToColor(graph, color)) {
-      printColorArray(color);
-      //return true;
+  std::pair<int,int> threadID_v = (std::pair<int,int>) * ((std::pair<int,int> * ) args);
+  int thread_number = threadID_v.first;
+  int v = threadID_v.second;
+
+  pthread_t p_id = pthread_self();
+  if (LOG_DATA) printf("[T%d]\tWating %ld, %d\n", thread_number, p_id, v);
+
+  int min_count = v;
+
+  int l = getLength(v);
+  for (int i=0; i<l/MAXTHREADS; i++) {
+    int id = i * MAXTHREADS + (thread_number%MAXTHREADS);
+    std::pair<int,int> c_j = getItem(v, id);
+    int c = c_j.second;
+    int j = c_j.first;
+    if (c < min_count) {
+      std::string col = padZeros(decToBase(j, c), v);
+      
+      if (LOG_DATA) printf("[T%d]\t(%d, %d)\t->\t%s\n", thread_number, c, j, col.c_str());
+      std::vector<int> colors(v, 0);
+      for (int t=0; t<v; t++) {
+        colors[t] = col.at(t) - '0';
+      }
+
+      assert(col.length()==v);
+      if (isSafeToColor(graph, colors)) {
+        min_count = c;
+      }
     }
-    return false;
   }
-  for (int j = 1; j <= m; j++) {
-    color[i] = j;
-    if (graphColoring(graph, m, i + 1, color))
-      return true;
- 
-    color[i] = 0;
-  }
- 
-  return false;
+
+  if (LOG_DATA) printf("[T%d]\t min=%d\n", thread_number, min_count);
+
+  return NULL;
 }
 
-void colorIt(Graph_t g) {
-	vector < vector < int >> graph(V, vector<int>(V,0));
-	for (int i=0; i<V; i++) {
-		for (int j = 0; j<V; j++)
-		{
-			graph[i][j] = g.adj[i][j];
-		}
-		
-	}
+int main(int argc, char ** argv) {
 
-	vector <int> color(V,0);
-	for (int i=0; i<V; i++) {
-		color[i] = 0;
-	}
+  int v;
+  std::cin >> v;
 
-	graphColoring(graph, V, 0, color);
-}
+  graph = std::vector<std::vector <int>>(v, std::vector<int>(v, 0));
 
-int main()
-{
-	Graph_t graph_ele1;
-
-	//init color
-
-	for (int y = 0; y < V; y++) {
-		graph_ele1.colors[y] = 99;
-		graph_ele1.colored[y] = false;
-	}
-
-	int N;
-	N = V;
-    bool graph_ele_in[N][N];
-
-    int u;
-    for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-        	cin >> u;
-			graph_ele_in[i][j] = u;
-		}
+  for (int i=0; i<v; i++) {
+    for (int j=0; j<v; j++) {
+      std::cin >> graph[i][j];
     }
+  }
 
-	//init graph
-	memcpy(&graph_ele1.adj, &graph_ele_in, sizeof(graph_ele1.adj));
+  // Create all the threads
+  for (int i = 0; i < MAXTHREADS; i++)
+  {
+    pthread_t thread_number;
+    printf("[TM]\tCreating thread %d \n", i);
+    t_thread_numbers[i] = std::make_pair(i, v);
+    pthread_create( & thread_number, NULL, handleDataArg, (void * ) & t_thread_numbers[i]);
+    THREADS[i] = thread_number;
+  }
 
-	colorIt(graph_ele1);
+  for (int i = 0; i < MAXTHREADS; i++)
+  {
+    pthread_t thread_id = THREADS[i];
+    pthread_join(thread_id, NULL);
+    printf("[INT] Stopped thread %d \n", i);
+  }
 
-	cout<<"min_colors: "<<min_colors<<endl;
-
-	return 0;
 }
